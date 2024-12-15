fbbe15fcc1dd4904212330fbc4e023f53b3e6eee
Kassim Zaid <81573513+kzaid0767@users.noreply.github.com>
2024-12-15 07:14:43 -0500

    with app

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -3,44 +3,50 @@ import x from '../assets/x.png'
 import o from '../assets/o.png'
 import Board from "./Board"
 import { useState } from "react"
-export default function Game(){
+const Game = () => {
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const [stepNumber, setStepNumber] = useState(0);
+    const [xIsNext, setXisNext] = useState(true);
+    const winner = calculateWinner(history[stepNumber]);
 
-    /* the state of the board to start the game */
-    const [board, setBoard] = useState(Array(9).fill(null))
-    const [xIsNext, setXisNext] = useState(true)
-    const winner = calculateWinner(board)
-    console.log(winner)
-
-    function handleClick(i){
-        /* making a copy of board state */
-        const boardCopy = [...board]
-        console.log(board[i])
-        /* if game won and space occupied then return */
-        if(winner || boardCopy[i]){
-            return
-        }
-        /* other fill with x or 0 */
-        boardCopy[i] = xIsNext? 'x' : 'o'
-        setBoard(boardCopy)
-        setXisNext(prevState => !prevState)
+    const handleClick = i => {
+        const timeInHistory = history.slice(0, stepNumber + 1);
+        const current = timeInHistory[stepNumber];
+        const squares = [...current];
+        // If user click an occupied square or if game is won, return
+        if (winner || squares[i]) return;
+        // Put an X or an O in the clicked square
+        squares[i] = xIsNext ? 'X' : 'O';
+        setHistory([...timeInHistory, squares]);
+        setStepNumber(timeInHistory.length);        
+        setXisNext(!xIsNext);
     }
 
-    const jumpTo = () => {
+    const jumpTo = step => {
+        setStepNumber(step);
+        setXisNext(step % 2 === 0)
+    };
 
-    }
-
-    const renderMoves = ()=> <button className='bg-slate-100 p-4 border-solid border-2 border-blue-200
-        cursor-pointer' onClick={() => setBoard(Array(9).fill(null))}> New Game </button>
-
-    
+    const renderMoves = () => (
+        history.map((_step, move) => {
+            const destination = move ? `Go to move#${move}` : 'Go to start';
+            return (
+                <li key={move}>
+                    <button onClick={() => jumpTo(move)}>{destination}</button>
+                </li>
+            )
+        })        
+    )
 
     return (
-        <div className="p-4">
-            <Board squares={board} onClick={handleClick} />
-            <div className='w-32 mx-auto mt-8'>
+        <>
+            <Board squares={history[stepNumber]} onClick={handleClick} />
+            <div>
                 <p>{winner ? 'Winner: ' + winner : 'Next Player: ' + (xIsNext ? 'X' : 'O')}</p>
-                {winner && renderMoves()}
+                {renderMoves()}
             </div>
-        </div>
+        </>
     )
 }
+
+export default Game;
